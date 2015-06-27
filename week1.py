4bd33f0782505dca8be870111d57866aa3909d8d
pyramidsnail <sherry.bim@gmail.com>
2015-06-27 14:55:37 +0200

    graph

--- week1.py
+++ week1.py
@@ -1,4 +1,47 @@
 """
+Provided code for Application portion of Module 1
+
+Imports physics citation graph 
+"""
+
+# general imports
+import urllib2, math
+import matplotlib.pyplot as plt
+# Set timeout for CodeSkulptor if necessary
+#import codeskulptor
+#codeskulptor.set_timeout(20)
+
+
+###################################
+# Code for loading citation graph
+
+CITATION_URL = "http://storage.googleapis.com/codeskulptor-alg/alg_phys-cite.txt"
+
+def load_graph(graph_url):
+    """
+    Function that loads a graph given the URL
+    for a text representation of the graph
+    
+    Returns a dictionary that models a graph
+    """
+    graph_file = urllib2.urlopen(graph_url)
+    graph_text = graph_file.read()
+    graph_lines = graph_text.split('\n')
+    graph_lines = graph_lines[ : -1]
+    
+    print "Loaded graph with", len(graph_lines), "nodes"
+    
+    answer_graph = {}
+    for line in graph_lines:
+        neighbors = line.split(' ')
+        node = int(neighbors[0])
+        answer_graph[node] = set([])
+        for neighbor in neighbors[1 : -1]:
+            answer_graph[node].add(int(neighbor))
+
+    return answer_graph
+
+"""
 project for WEEK1
 """
 EX_GRAPH0={0:set([1,2]),
@@ -66,15 +109,109 @@ def in_degree_distribution(digraph):
         res[dic[out_node]] += 1
     return res
 
+import random
 
-# if __name__ == '__main__':
+
+class DPATrial:
+    """
+    Simple class to encapsulate optimized trials for DPA algorithm
+    
+    Maintains a list of node numbers with multiple instances of each number.
+    The number of instances of each node number are
+    in the same proportion as the desired probabilities
+    
+    Uses random.choice() to select a node number from this list for each trial.
+    """
+
+    def __init__(self, num_nodes):
+        """
+        Initialize a DPATrial object corresponding to a 
+        complete graph with num_nodes nodes
+        
+        Note the initial list of node numbers has num_nodes copies of
+        each node number
+        """
+        self._num_nodes = num_nodes
+        self._node_numbers = [node for node in range(num_nodes) for dummy_idx in range(num_nodes)]
+
+
+    def run_trial(self, num_nodes):
+        """
+        Conduct num_node trials using by applying random.choice()
+        to the list of node numbers
+        
+        Updates the list of node numbers so that the number of instances of
+        each node number is in the same ratio as the desired probabilities
+        
+        Returns:
+        Set of nodes
+        """
+        
+        # compute the neighbors for the newly-created node
+        new_node_neighbors = set()
+        for dummy_idx in range(num_nodes):
+            new_node_neighbors.add(random.choice(self._node_numbers))
+        
+        # update the list of node numbers so that each node number 
+        # appears in the correct ratio
+        self._node_numbers.append(self._num_nodes)
+        self._node_numbers.extend(list(new_node_neighbors))
+        
+        #update the number of nodes
+        self._num_nodes += 1
+        return new_node_neighbors
+    # def update_graph(self):
+    
+if __name__ == '__main__':
     # test = {0:[1,2],1:[2,3]}
     # res1 = compute_in_degrees(test)
     # res2 = in_degree_distribution(test)
     # res3 = make_complete_graph(3)
     # print 
         
-    
+    # citation_graph = load_graph(CITATION_URL)
+    # dis = in_degree_distribution(citation_graph)
+    # #####  normalization
+    # dis_nor = {}
+    # total = 0
+    # for i in dis:
+    #     total += dis[i]
+
+    # for i in dis:
+    #     dis_nor[i] = 1.0*dis[i]/total
+
+    # plt.plot([math.log(i  if i>0 else 1, 10) for i in dis_nor.keys()], [math.log(i, 10) for i in dis_nor.values()], 'ro')
+    # plt.xlabel('log10 of citation numbers')
+    # plt.ylabel('log10 of the normalized in-degree distribution')
+    # plt.title('in-degree distribution of citation graph') 
+    # plt.show()
+
+    dpa = DPATrial(99)
+    for i in xrange(181):
+        dpa.run_trial(99)
+
+    dis = dict((i,0) for i in xrange(280))
+    for i in dpa._node_numbers:
+        dis[i] += 1
+
+
+    dis_nor = {}
+    total = 0
+    for i in dis:
+        total += dis[i]
+
+    for i in dis:
+        dis_nor[i] = 1.0*dis[i]/total
+
+    plt.plot([math.log(i  if i>0 else 1, 10) for i in dis_nor.keys()], [math.log(i, 10) for i in dis_nor.values()], 'ro')
+    plt.xlabel('log10 of in-degree numbers')
+    plt.ylabel('log10 of the normalized in-degree distribution')
+    plt.title('in-degree distribution of DPA') 
+    plt.show()
+
             
     
         
+
+
+
--- week2.py
+++ week2.py
@@ -1,7 +1,162 @@
 '''
 code for the project of WEEK2
 '''
-# import sys, os, re
+import sys, os, re
+import urllib2
+import random
+import time
+import math
+import matplotlib.pyplot as plt
+import random
+
+
+def make_complete_graph(num_nodes):
+    """
+    returns a dictionary corresponding to a complete directed graph
+    with the specified number of nodes
+    """
+    complete_graph = {}
+    if num_nodes<1:
+        return complete_graph
+    else:
+        complete_graph = dict((x,[]) for x in xrange(num_nodes))
+        for out_node in xrange(num_nodes):
+            for in_node in xrange(num_nodes):
+                if out_node!=in_node:
+                    complete_graph[out_node].append(in_node)
+
+        for key in complete_graph:
+            complete_graph[key] = set(complete_graph[key])
+    return complete_graph
+
+class UPATrial:
+    """
+    Simple class to encapsulate optimizated trials for the UPA algorithm
+    
+    Maintains a list of node numbers with multiple instance of each number.
+    The number of instances of each node number are
+    in the same proportion as the desired probabilities
+    
+    Uses random.choice() to select a node number from this list for each trial.
+    """
+
+    def __init__(self, num_nodes):
+        """
+        Initialize a UPATrial object corresponding to a 
+        complete graph with num_nodes nodes
+        
+        Note the initial list of node numbers has num_nodes copies of
+        each node number
+        """
+        self._num_nodes = num_nodes
+        self._node_numbers = [node for node in range(num_nodes) for dummy_idx in range(num_nodes)]
+
+
+    def run_trial(self, num_nodes):
+        """
+        Conduct num_nodes trials using by applying random.choice()
+        to the list of node numbers
+        
+        Updates the list of node numbers so that each node number
+        appears in correct ratio
+        
+        Returns:
+        Set of nodes
+        """
+        
+        # compute the neighbors for the newly-created node
+        new_node_neighbors = set()
+        for _ in range(num_nodes):
+            new_node_neighbors.add(random.choice(self._node_numbers))
+        
+        # update the list of node numbers so that each node number 
+        # appears in the correct ratio
+        self._node_numbers.append(self._num_nodes)
+        for dummy_idx in range(len(new_node_neighbors)):
+            self._node_numbers.append(self._num_nodes)
+        self._node_numbers.extend(list(new_node_neighbors))
+        
+        #update the number of nodes
+        self._num_nodes += 1
+        return new_node_neighbors
+
+def copy_graph(graph):
+    """
+    Make a copy of a graph
+    """
+    new_graph = {}
+    for node in graph:
+        new_graph[node] = set(graph[node])
+    return new_graph
+
+def delete_node(ugraph, node):
+    """
+    Delete a node from an undirected graph
+    """
+    neighbors = ugraph[node]
+    ugraph.pop(node)
+    for neighbor in neighbors:
+        ugraph[neighbor].remove(node)
+    
+def targeted_order(ugraph):
+    """
+    Compute a targeted attack order consisting
+    of nodes of maximal degree
+    
+    Returns:
+    A list of nodes
+    """
+    # copy the graph
+    new_graph = copy_graph(ugraph)
+    
+    order = []    
+    while len(new_graph) > 0:
+        max_degree = -1
+        for node in new_graph:
+            if len(new_graph[node]) > max_degree:
+                max_degree = len(new_graph[node])
+                max_degree_node = node
+        
+        neighbors = new_graph[max_degree_node]
+        new_graph.pop(max_degree_node)
+        for neighbor in neighbors:
+            new_graph[neighbor].remove(max_degree_node)
+
+        order.append(max_degree_node)
+    return order
+    
+
+
+##########################################################
+# Code for loading computer network graph
+
+NETWORK_URL = "http://storage.googleapis.com/codeskulptor-alg/alg_rf7.txt"
+
+
+def load_graph(graph_url):
+    """
+    Function that loads a graph given the URL
+    for a text representation of the graph
+    
+    Returns a dictionary that models a graph
+    """
+    graph_file = urllib2.urlopen(graph_url)
+    graph_text = graph_file.read()
+    graph_lines = graph_text.split('\n')
+    graph_lines = graph_lines[ : -1]
+    
+    print "Loaded graph with", len(graph_lines), "nodes"
+    
+    answer_graph = {}
+    for line in graph_lines:
+        neighbors = line.split(' ')
+        node = int(neighbors[0])
+        answer_graph[node] = set([])
+        for neighbor in neighbors[1 : -1]:
+            answer_graph[node].add(int(neighbor))
+
+    return answer_graph
+
 
 def bfs_visited(graph, start_node):
     '''
@@ -66,10 +221,55 @@ def compute_resilience(graph, attack_order):
         
 
 
+def make_ER_graph(num_nodes, p):
+    """
+    returns a dictionary corresponding to a complete directed graph
+    with the specified number of nodes
+    """
+    ER_graph = {}
+    if num_nodes<1:
+        return ER_graph
+    else:
+        ER_graph = dict((x,[]) for x in xrange(num_nodes))
+        for out_node in xrange(num_nodes):
+            for in_node in xrange(num_nodes):
+                if out_node!=in_node and random.uniform(0,1)<p:
+                    ER_graph[out_node].append(in_node)
+                    ER_graph[in_node].append(out_node)
+
+        for key in ER_graph:
+            ER_graph[key] = set(ER_graph[key])
+    return ER_graph
 
 
 
-# if __name__ == '__main__':
+if __name__ == '__main__':
+    answer_graph = load_graph(NETWORK_URL)
+    er_graph = make_ER_graph(1239, 0.001)
+
+    #####  for the UPA graph
+    ##### sum of edges 2476; sum of nodes: 1239
+    upa_graph = make_complete_graph(2) 
+    upa = UPATrial(2)
+    for i in xrange(2, 1239):
+        upa_graph[i] = []
+        new_node_neighbors = upa.run_trial(2)
+        for node in new_node_neighbors:
+            upa_graph[i].append(node)
+        upa_graph[i].extend(new_node_neighbors)
+
+    keys = random.sample(range(1239), 1239)
+    answer_keys = random.sample(answer_graph.keys(), len(answer_graph))
+    answer = compute_resilience(answer_graph, answer_keys)
+    er = compute_resilience(er_graph, keys)
+    upa = compute_resilience(upa_graph, keys)
+
+    plt.plot(range(1240), answer, range(1240), er, range(1240), upa, linestyle='-')
+    plt.show()
+
+
+        
+        
 #     GRAPH0 = {0: set([1]),
 #               1: set([0, 2]),
 #               2: set([1, 3]),
